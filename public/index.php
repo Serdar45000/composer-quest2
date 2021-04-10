9af1b17bd6ecefe33382cca9990e238b53f296bb
Serdar45000 <serdar45000@gmail.com>
2021-04-11 00:03:35 +0200

    5.2 Composer 2 - Package Management

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 14:01
+ */
+
+ use HelloWorld\SayHello;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+echo SayHello::world();
